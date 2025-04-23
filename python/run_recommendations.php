4d21013f91890ff835c5444dea17ff999cf54e63
DAYSAK ESSID <130565564+daysak21@users.noreply.github.com>
2025-04-23 17:57:40 +0200

    Delete python/run_recommendations.php

--- python/run_recommendations.php
+++
@@ -1,9 +0,0 @@
-<?php
-$python_path = '/usr/bin/python3'; 
-$script_path = __DIR__ . '/recommendation_minmax.py';
-
-$command = escapeshellcmd("$python_path $script_path");
-$output = shell_exec($command);
-
-echo "<pre>$output</pre>";
-?>
