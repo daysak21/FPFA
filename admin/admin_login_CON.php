c52971deeeddac7ea18976d96b377c43f688fe99
ceyf01 <sayfmhj@gmail.com>
2025-04-24 23:24:03 +0100

    fixed sql injection vulnerability in admin login

--- admin/admin_login_CON.php
+++ admin/admin_login_CON.php
@@ -6,21 +6,26 @@ session_start();
 if (isset($_POST['admin_login'])) {
     $username = $_POST['username'];
     $password = $_POST['password'];
-    $select_query = "SELECT * FROM `admin_table` WHERE admin_name='$username'";
-    $select_result = mysqli_query($con, $select_query);
-    $row_data = mysqli_fetch_assoc($select_result);
-    $row_count = mysqli_num_rows($select_result);
-    //user check about username & pass
-    if ($row_count > 0) {
+
+    $stmt = $con->prepare("SELECT * FROM admin_table WHERE admin_name = ?");
+    $stmt->bind_param("s", $username);
+    $stmt->execute();
+    $result = $stmt->get_result();
+
+    if ($result->num_rows === 1) {
+        $row_data = $result->fetch_assoc();
+
         if (password_verify($password, $row_data['admin_password'])) {
             $_SESSION['admin_username'] = $username;
             echo "<script>alert('Login Successfully');</script>";
             echo "<script>window.open('./index.php','_self');</script>";
         } else {
-            echo "<script>alert('Invalid Credentials')</script>";
+            echo "<script>alert('Invalid Credentials');</script>";
         }
     } else {
-        echo "<script>alert('Username does not exist')</script>";
+        echo "<script>alert('Username does not exist');</script>";
     }
+
+    $stmt->close();
 }
 ?>
